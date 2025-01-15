fdb6374f76f030740cf71d51efb2b8d89d821bcb
smkumaill <smkumaill@gmail.com>
2025-01-15 19:36:08 +0400

    fixed setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='panscan',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
-    scripts=['scripts/script1.pl', 'scripts/script2.pl'],
+    scripts=['scripts/findNovelSeq.pl', 'scripts/preprocessVCF.pl'],
     entry_points={
         'console_scripts': [
-            'scanpy=scanpy.__main__:main',
+            'panscan=panscan.__main__:main',
         ],
     },
     install_requires=[
