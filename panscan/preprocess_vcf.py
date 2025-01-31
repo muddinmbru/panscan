8343123d8d759a975648aff065269c2aad70e82f
shehzad-99 <63859730+shehzad-99@users.noreply.github.com>
2025-01-31 11:10:38 +0400

    Update preprocess_vcf.py

--- panscan/preprocess_vcf.py
+++ panscan/preprocess_vcf.py
@@ -1,9 +1,22 @@
 import subprocess
-import sys
+import os
 import argparse
 
 def run_preprocess_vcf(vcf_file):
-    subprocess.run(['perl', 'scripts/preprocessVCF.pl', '-i', vcf_file])
+    # Get the absolute path of the script directory
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    # Construct the absolute path to preprocessVCF.pl
+    perl_script = os.path.join(script_dir, "..", "scripts", "perlModules", "preprocessVCF.pl")
+    
+    # Normalize the path
+    perl_script = os.path.abspath(perl_script)
+
+    # Debugging print (optional)
+    print(f"Running Perl script at: {perl_script}")
+
+    # Run the Perl script
+    subprocess.run(['perl', perl_script, '-i', vcf_file], check=True)
 
 def main(args):
     vcf_file = args.vcf
@@ -11,7 +24,6 @@ def main(args):
     run_preprocess_vcf(vcf_file)
 
 def add_subparser(subparsers):
-    # Add the subcommand 'preprocess_vcf'
     subparser = subparsers.add_parser('preprocess_vcf', help="Preprocess VCF file by normalizing, decomposing, and merging genotypes.")
     subparser.add_argument("vcf", help="Path to the input VCF file.")
     subparser.set_defaults(func=main)
