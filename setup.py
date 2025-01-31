8b4ec7f2a1010bf8e3e01a142feb5734a7448cd2
shehzad-99 <63859730+shehzad-99@users.noreply.github.com>
2025-01-31 12:21:00 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='panscan',
     version='0.3.2',
     packages=find_packages(),
-    scripts=['scripts/preprocessVCF.pl', 'scripts/findUniqVariants.pl'],
+    scripts=['scripts/preprocessVCF.pl', 'scripts/findUniqVariants.pl', 'scripts/findNovelSeq.pl'],
     entry_points={
         'console_scripts': [
             'scanpy=scanpy.__main__:main',
