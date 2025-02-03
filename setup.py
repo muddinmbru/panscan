2188f720f5d2f41e863f979ae359eb8953c3c77c
shehzad-99 <63859730+shehzad-99@users.noreply.github.com>
2025-02-03 15:52:30 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
+  GNU nano 4.8                                                                                                      setup.py                                                                                                                 
 from setuptools import setup, find_packages
 
 setup(
-    name='panscan',
-    version='0.3.2',
+    name="panscan",
+    version="0.3.2",
     packages=find_packages(),
-    scripts=['scripts/preprocessVCF.pl', 'scripts/findUniqVariants.pl', 'scripts/findNovelSeq.pl'],
+    package_data={
+        "panscan": [
+            "scripts/*",              # Include all files in the scripts directory
+            "scripts/perlModules/*"    # Include all files in the perlModules directory
+        ]
+    },
+    include_package_data=True,
     entry_points={
-        'console_scripts': [
-            'scanpy=scanpy.__main__:main',
+        "console_scripts": [
+            "panscan=panscan.__main__:main",  # Assuming you have a `__main__.py` in `panscan/`
         ],
     },
     install_requires=[
-        "pandas"
+        "pandas",
     ],
 )
